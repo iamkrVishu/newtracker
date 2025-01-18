2abc74e7ce2e75c56088221681356e501c2aa3f1
Vishvajeet Kumar <kumarvishvajeetsingh@gmail.com>
2025-01-19 04:48:45 +0530

    Update frontend and backend

--- frontend/src/Component/Auth.js
+++ frontend/src/Component/Auth.js
@@ -11,7 +11,7 @@ const Auth = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate(); // Initialize navigate
 
-  const BACKEND_URL = "https://newtracker-backend.onrender.com/api/auth"; // Update with the actual backend URL
+  const BACKEND_URL = "https://newtracker-backend.onrender.com/api/auth"; // Replace with your actual backend URL
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -19,7 +19,7 @@ const Auth = () => {
       .post(`${BACKEND_URL}/signin`, { email, password })
       .then((response) => {
         alert(`Logged in successfully!`);
-        window.location.replace("https://newtracker-zeta.vercel.app/"); // Redirect to frontend app
+        window.location.replace("https://habit-tracker-lac.vercel.app/"); // Use replace to force a redirect
       })
       .catch((err) => {
         setErrorMessage("Invalid credentials. Please try again.");
@@ -33,7 +33,7 @@ const Auth = () => {
       .post(`${BACKEND_URL}/signup`, { name, email, password })
       .then((response) => {
         alert(`Account created successfully!`);
-        window.location.replace("https://newtracker-zeta.vercel.app/"); // Redirect to frontend app
+        window.location.replace("https://habit-tracker-lac.vercel.app/"); // Redirect to external link
       })
       .catch((err) => {
         setErrorMessage("Error creating account. Please try again.");
